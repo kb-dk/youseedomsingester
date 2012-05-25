a17c17f95fe6708b743db48ffea8061a3c3e93aa
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2012-05-25 08:35:40 +0200

    Some refactoring to make more modular and testable

---
+++ src/main/java/dk/statsbiblioteket/doms/yousee/OptionParser.java
@@ -0,0 +1,11 @@
+package dk.statsbiblioteket.doms.yousee;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kfc
+ * Date: 5/25/12
+ * Time: 7:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OptionParser {
+}
